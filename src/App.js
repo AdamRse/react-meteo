caae80e77f888f89a283c5f25cb044c4110c5a01
AdamRse <bakaarion3@gmail.com>
2024-05-22 16:49:29 +0200

    oui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Meteo from "./components/Meteo"
+import Navbar from "./components/Navbar"
+// const Header = () => {
+//   return(
+//       <nav className="header-links">
+//           <Link to="/" className="link">Home</Link>
+//           <Link to="/about" className="link">About</Link>
+//           <Link to="/contact" className="link">Contact Us</Link>
+//       </nav>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Meteo/>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+        <>
+        <header class="App-header"><img src="logo_transparent.png" class="App-logo" alt="logo"/></header>
+        </>
+    )
+  }
+}
